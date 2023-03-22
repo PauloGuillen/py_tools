9f7a0652e5e1ac0ec172cdfd3d439b3b5daa7d7e
PauloGuillen <psm.guillen@gmail.com>
2023-03-22 15:17:32 -0300

    test parametros

--- tests/test_spam/test_usuarios.py
+++ tests/test_spam/test_usuarios.py
@@ -1,33 +1,15 @@
-import pytest
-from py_tools.spam.db import Conexao
 from py_tools.spam.model import Usuario
 
 
-@pytest.fixture
-def conexao():
-    # Setup
-    conexao_obj = Conexao()
-    yield conexao_obj
-    # Tear Down
-    conexao_obj.fechar()
-
-
-@pytest.fixture
-def sessao(conexao):
-    sessao_obj = conexao.gerar_sessao()
-    yield sessao_obj
-    sessao_obj.roll_back()
-    sessao_obj.fechar()
-
-
 def test_salvar_usuario(sessao):
-    usuario = Usuario(nome='usuario_teste')
+    usuario = Usuario(nome='usuario_teste', email='usuario_teste@gmail.com')
     sessao.salvar(usuario)
     assert isinstance(usuario.id, int)
 
 
 def test_listar_usuarios(sessao):
-    usuarios = [Usuario(nome='usuario_teste'), Usuario(nome='Outro_usuario')]
+    usuarios = [Usuario(nome='usuario_teste', email='usuario_teste@gmail.com'),
+                Usuario(nome='Outro_usuario', email='Outro_usuario@gmail.com')]
     for usuario in usuarios:
         sessao.salvar(usuario)
     assert usuarios == sessao.listar()
