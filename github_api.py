9f273265ba534dc5f35d5f6d22cececf37f2f623
PauloGuillen <psm.guillen@gmail.com>
2023-03-22 22:02:41 -0300

    pytest mock

--- github_api.py
+++ github_api.py
@@ -3,10 +3,5 @@ import requests
 
 def buscar_avatar(usuario):
     url = f'https://api.github.com/users/{usuario}'
-
     resposta = requests.get(url)
     return resposta.json()['avatar_url']
-
-
-if __name__ == '__main__':
-    print(buscar_avatar('renzon'))
