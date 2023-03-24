f94550ead68f5652f09d9aff27b5d298bd15f3d1
PauloGuillen <psm.guillen@gmail.com>
2023-03-24 19:13:48 -0300

    pytest example data

---
+++ src/db_connection.py
@@ -0,0 +1,11 @@
+import time
+
+
+class DBConnector:
+    def __init__(self):
+        time.sleep(2)
+        pass
+
+    def get(self, id):
+        time.sleep(2)
+        return f'some data. id: {id}'
---
+++ src/employee.py
@@ -0,0 +1,15 @@
+import requests
+
+
+class Employee:
+    def __init__(self, first, last, pay):
+        self.first = first
+        self.last = last
+        self.pay = pay
+
+    def monthly_schedule(self, month):
+        response = requests.get(f'http://company.com/{self.last}/{month}')
+        if response.ok:
+            return response.text
+        else:
+            return 'Bad Response'
